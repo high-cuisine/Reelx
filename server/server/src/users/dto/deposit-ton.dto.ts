c327ac2d5eb46c131d33150aa60c909e93a809fe
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2026-01-20 17:10:58 +0400

    fix dto

--- server/server/src/users/dto/deposit-ton.dto.ts
+++ server/server/src/users/dto/deposit-ton.dto.ts
@@ -1,3 +1,9 @@
+import { IsNumber, IsPositive } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class DepositTonDto {
+  @Type(() => Number)
+  @IsNumber()
+  @IsPositive()
   amount: number;
 }
