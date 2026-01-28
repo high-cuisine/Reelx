6e5f6dd806484711ffe8d8cbe168ed9f4aee0695
Александр Носиков <aleksandrnosikov@MacBook-Pro-Aleksandr.local>
2026-01-28 13:30:48 +0400

    fix dto

--- server/server/src/promocode/controllers/promocode.controller.ts
+++ server/server/src/promocode/controllers/promocode.controller.ts
@@ -1,9 +1,12 @@
 import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { IsNotEmpty, IsString } from 'class-validator';
 import { PromocodeService } from '../service/promocode.service';
 import { JwtAuthGuard } from '../../../libs/common/guard/jwt-auth.guard.guard';
 import { CurrentUser } from '../../../libs/common/decorators/current-user.decorator';
 
 class UsePromocodeDto {
+  @IsString()
+  @IsNotEmpty()
   promocode: string;
 }
 
